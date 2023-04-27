bd13933dc9738b768f8ae13dd645cd5c390696ba
João Vitor Gomes Dantas <92762467+JoaoVitorGomesDantas@users.noreply.github.com>
2023-04-27 20:20:33 -0300

    Update question04.sql #1

--- Tarefa01/question04.sql
+++ Tarefa01/question04.sql
@@ -3,8 +3,9 @@
 select f.nome, f.salario, d.descricao
 from funcionario f
   join departamento d on f.cod_depto = d.codigo
-where f.codigo <> all (
+where f.codigo not in (
   select d.cod_gerente
   from departamento d
-  where d.cod_gerente is not null);
+  where d.cod_gerente is not null)
+  order by cod_depto;
   

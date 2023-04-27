afaafe8872d2fd48019865264cc3464974f33d55
João Vitor Gomes Dantas <92762467+JoaoVitorGomesDantas@users.noreply.github.com>
2023-04-27 20:34:11 -0300

    Added the answer to question 07 #1

---
+++ Tarefa01/question07.sql
@@ -0,0 +1,9 @@
+## Resposta da Questão 07
+
+CREATE VIEW func_depto AS
+SELECT COUNT(f.codigo) AS qtd, f.cod_depto
+FROM funcionario f
+GROUP BY f.cod_depto;
+SELECT d.descricao AS departamento, g.nome AS gerente, fd.qtd AS funcionarios
+FROM func_depto fd LEFT JOIN departamento d ON d.codigo=fd.cod_depto
+LEFT JOIN funcionario g ON d.cod_gerente = g.codigo;

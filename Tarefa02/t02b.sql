85885c6aeb966dedcc21ca99c2f470474d18968f
João Vitor Gomes Dantas <92762467+JoaoVitorGomesDantas@users.noreply.github.com>
2023-05-16 09:06:30 -0300

    Update by adapting the function #2

--- Tarefa02/t02b.sql
+++ Tarefa02/t02b.sql
@@ -1,3 +1,19 @@
+CREATE FUNCTION idade_funcionario (codigo_funcionario int)
+RETURN int
+LANGUAGE plpgsql
+AS $$
+DECLARE
+      idade_funcionario integer;
+BEGIN
+      SELECT DATE_PART('YEAR', AGE(dataNasc))
+      INTO idade_funcionario
+      FROM funcionario
+      WHERE codigo = codigo_funcionario;
+      
+      RETURN idade;
+END;
+$$;
+
 CREATE OR REPLACE FUNCTION media_idade_departamento (codigo_funcionario int, codigo_departamento int)
 RETURN float
 LANGUAGE plpgsql
@@ -5,11 +21,7 @@ AS $$
 DECLARE
       media_idade_departamento float;
 BEGIN
-      SELECT AVG(
-            SELECT DATE_PART('YEAR', AGE(dataNasc))
-            FROM funcionario
-            WHERE codigo = codigo_funcionario;
-      )
+      SELECT AVG(idade_funcionario(funcionario.codigo))
       INTO media_idade_departamento
       FROM funcionario
       WHERE funcionario.depto = codigo_departamento;

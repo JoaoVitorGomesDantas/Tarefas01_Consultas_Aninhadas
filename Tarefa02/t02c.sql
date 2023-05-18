d0646ddb406a99b806807f8cb9f7e013467cfffc
João Vitor Gomes Dantas <92762467+JoaoVitorGomesDantas@users.noreply.github.com>
2023-05-17 22:07:54 -0300

    Solution to question 2 letter c #2

---
+++ Tarefa02/t02c.sql
@@ -0,0 +1,43 @@
+CREATE FUNCTION idade_funcionario (codigo_funcionario int)
+RETURN int
+LANGUAGE plpgsql
+AS $$
+DECLARE
+      idade_funcionario integer;
+BEGIN
+      SELECT DATE_PART('YEAR', AGE(dataNasc))
+      INTO idade_funcionario
+      FROM funcionario
+      WHERE codigo = codigo_funcionario;
+      
+      RETURN idade;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION media_idade_departamento (codigo_departamento int)
+RETURN float
+LANGUAGE plpgsql
+AS $$
+DECLARE
+      media_idade_departamento float;
+BEGIN
+      SELECT AVG(idade_funcionario(funcionario.codigo))
+      INTO media_idade_departamento
+      FROM funcionario
+      WHERE funcionario.depto = codigo_departamento;
+      
+      RETURN media_idade_departamento;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION  idade_maior_media_departamento ()
+RETURN table (codigo integer, nome varchar)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+      RETURN query
+        SELECT funcionario.codigo, funcionario.nome
+        FROM funcionario
+        WHERE idade_funcionario(funcionario.codigo) > media_idade_departamento(funcionario.depto);
+END;
+$$;

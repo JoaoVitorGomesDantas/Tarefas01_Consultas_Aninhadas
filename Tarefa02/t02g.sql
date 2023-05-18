b4b9d219d05427ef08651df7a5886ffd9e4594d5
João Vitor Gomes Dantas <92762467+JoaoVitorGomesDantas@users.noreply.github.com>
2023-05-18 18:59:27 -0300

    Variable name changes #2
    Changes to the variable name so as not to conflict with the table name.

--- Tarefa02/t02g.sql
+++ Tarefa02/t02g.sql
@@ -3,16 +3,16 @@ RETURNS int
 LANGUAGE plpgsql
 AS $$
 DECLARE
-	    atividade_membro integer;
+	    quantidade_atividade integer;
 BEGIN
       SELECT COUNT(atividade.codigo)
-	    INTO atividade_membro
-	    FROM projeto
-		    JOIN membro ON projeto.equipe = membro.codEquipe
-		    JOIN atividade_membro ON atividade_membro.codMembro = membro.codigo
-		    JOIN atividade ON atividade_membro.codAtividade = atividade.codigo
-	    WHERE projeto.codigo = codigo_projeto AND membro.codFuncionario = codigo_funcionario;
-	
-	    RETURN atividade_membro;
+      INTO quantidade_atividade
+      FROM projeto
+      		JOIN membro ON projeto.equipe = membro.codEquipe
+      		JOIN atividade_membro ON atividade_membro.codMembro = membro.codigo
+      		JOIN atividade ON atividade_membro.codAtividade = atividade.codigo
+      WHERE projeto.codigo = codigo_projeto AND membro.codFuncionario = codigo_funcionario;
+      
+      RETURN quantidade_atividade;
 END;
 $$;

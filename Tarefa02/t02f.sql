77adba953a6a2c3c8c91c79758693c67443d53a3
João Vitor Gomes Dantas <92762467+JoaoVitorGomesDantas@users.noreply.github.com>
2023-05-18 18:53:24 -0300

    Code changes #2

--- Tarefa02/t02f.sql
+++ Tarefa02/t02f.sql
@@ -1,15 +1,19 @@
-CREATE OR REPLACE PROCEDURE equipe_projeto (codigo_projeto int)
-RETURNS table (funcionario character varying, departamento character varying)
+CREATE PROCEDURE equipe_projeto (codigo_projeto int)
 LANGUAGE plpgsql
 AS $$
+DECLARE
+      exibir record;
 BEGIN
-      RETURN query
-      SELECT funcionario.nome, departamento.sigla
-      FROM projeto
-        JOIN equipe ON projeto.equipe = equipe.codigo
-        JOIN membro ON projeto.equipe = membro.codEquipe
-        JOIN funcionario ON funcionario.codigo = membro.codFuncionario
-        LEFT JOIN departamento ON funcionario.depto = departamento.codigo
-      WHERE projeto.codigo = codigo_projeto;
+      FOR exibir IN
+            SELECT funcionario.nome, departamento.sigla
+            FROM projeto
+                  JOIN equipe ON projeto.equipe = equipe.codigo
+                  JOIN membro ON projeto.equipe = membro.codEquipe
+                  JOIN funcionario ON funcionario.codigo = membro.codFuncionario
+                  LEFT JOIN departamento ON funcionario.depto = departamento.codigo
+            WHERE projeto.codigo = codigo_projeto;
+      LOOP
+            RAISE NOTICE 'Funcionario: %, Departamento: %', exibir.nome, exibir.sigla;
+      END LOOP;
 END;
 $$;
